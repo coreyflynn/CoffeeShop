922099d6bcc4d6071ec0c4fd19b5a075db33c793
Corey Flynn <flynn.corey@gmail.com>
2013-10-29 15:53:08 -0400

    modified configure_fish, removed 32bit sig_tools

--- res/fish/configure_fish.sh
+++ res/fish/configure_fish.sh
@@ -3,6 +3,5 @@ chsh -s /usr/bin/fish vagrant
 mkdir -p /home/vagrant/.config/fish/functions
 cp /vagrant/res/fish/fish_prompt.fish /home/vagrant/.config/fish/functions/fish_prompt.fish
 echo "set -g -x fish_greeting 'Welcome to the Coffee Shop'" > /home/vagrant/.config/fish/config.fish
-echo 'set -x PYTHONPATH /vagrant/res/pestle/ $PYTHONPATH' >> /home/vagrant/.config/fish/config.fish
 echo 'set -x PATH /xchip/cogs/bin $PATH' >> /home/vagrant/.config/fish/config.fish
 echo 'set -x PATH /xchip/cogs/matlib/bin $PATH' >> /home/vagrant/.config/fish/config.fish
